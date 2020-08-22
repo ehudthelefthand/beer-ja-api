5880517dc47a71ea3e80d54e8ea217f1fb4fa923
Peerawat Poombua <peerawat@odds.team>
2020-08-22 21:37:53 +0700

    added case outline to beer crud

---
+++ src/models/__tests__/crud.spec.js
@@ -0,0 +1,8 @@
+describe('Beer CRUD', () => {
+    // describe('createBeer')
+    // describe('getBeerByID')
+    // describe('getAllBeer')
+    // describe('updateBeer')
+    // describe('deleteBeer')
+    // describe('reviewBeer')
+})
